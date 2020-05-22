aaeac4f14a1be87bcf9e7c393b944732c08b9983
emilme <emilme@sir.mle>
2020-05-22 11:04:45 -0700

    updated NavBar.js & Home.js skeleton data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <NavBar/>
+      <Home/>
+    </div>
   );
 };
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const HomePage = ()=>{
-    return <></>;
+    return (
+    <div>
+        <img src='./Pizza.jpg' alt='Pizza'/>
+        <h1>Delivered straight to you while you do Lambda!</h1>
+        <button>Order Pizza</button>
+    </div>
+    );
 };
 
 export default HomePage;
